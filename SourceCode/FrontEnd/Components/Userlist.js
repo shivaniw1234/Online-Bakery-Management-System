4124f43e485e9620a0fdd6051f687078bbd4dbda
Santosh jadhav <102609748+Santyjadhav007@users.noreply.github.com>
2022-09-18 00:32:05 +0530

    Add files via upload

---
+++ SourceCode/FrontEnd/Components/Userlist.js
@@ -0,0 +1,41 @@
+import { useState } from "react"
+import {useEffect} from "react";
+import axios from "axios";
+import React from "react";
+
+export default function Userlist() {
+let [user,setUser]=useState([])
+
+
+useEffect(() => {
+    axios.get("http://localhost:8080/user/getAll",{}).
+    then((res)=>{ setUser(res.data); console.log(res.data)}).
+    catch((err)=>{ console.log(err)});;
+ },[]);
+ 
+
+return (
+    <div>
+        <center><table border="2" bgcolor="yellow">
+            <thead>
+                <th>firstname</th>
+                <th>lastName</th>
+                <th>emailId</th>
+            </thead>
+            <tbody>
+            {user.map((ele)=>{  
+      return ( <tr><td> { ele.firstName}</td>
+          <td> {ele.lastName}</td>
+          <td>{ele.emailId}</td>
+          </tr>)  })}
+       </tbody>
+        </table></center>
+    </div>
+
+
+
+)
+
+
+
+}
