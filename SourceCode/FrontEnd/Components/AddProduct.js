2a40303973f319a7822267956673e89728169bbd
Santosh jadhav <102609748+Santyjadhav007@users.noreply.github.com>
2022-09-21 00:11:17 +0530

    Update AddProduct.js

--- SourceCode/FrontEnd/Components/AddProduct.js
+++ SourceCode/FrontEnd/Components/AddProduct.js
@@ -22,15 +22,15 @@ export default function AdminDash() {
             categoryName: category_name,
             stock: stock,
             sellingPrice: selling_price,
-            img:image
+            img: image
         }).then(Response => {
-                if (Response.status === 200) {
-                    alert(" Operation Succefully  ")
-                    navigate("/login")
-                } else {
-                    alert("Invalid Data format")
-                }
-            }).catch((error) => { alert(error.message)});
+            if (Response.status === 200) {
+                alert(" Operation Succefully  ")
+                navigate("/login")
+            } else {
+                alert("Invalid Data format")
+            }
+        }).catch((error) => { alert(error.message) });
 
     }
 
@@ -99,107 +99,80 @@ export default function AdminDash() {
 
 
             </header>
-            <section className="vh-100" style={{ "background-color": "#b6c6d8;" }}>
-                <div className="container py-5 h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col col-xl-10">
-                            <div className="card" style={{ "border-radius": "1rem;" }}>
-                                <div className="row g-0">
-                                    <div className="col-md-8 col-lg-6 d-none d-md-block">
-                                        <img src="assets/img/login.jpg" width="100%" height="100%"
-                                            alt="login form" style={{ "border-radius": "1rem 0 0 1rem;" }} />
-                                    </div>
-                                    <div className="col-md-4 col-lg-6 d-flex align-items-center">
-                                        <div className="card-body p-4 p-lg-5 text-black">
+
+            <body >
+                <section className="vh-100" style={{ "background-color": "#b6c6d8;" }}>
+                    <div className="container py-5 h-100" >
+                        <div className="row d-flex justify-content-center align-items-center h-100">
+                            <div className="col col-xl-10">
+                                <div className="card" style={{ "border-radius": "1rem;" }}>
+                                    <div className="row g-0">
+                                        <div className="col-md-8 col-lg-6 d-none d-md-block">
+                                            <img src="../assets/images/bakery-login.jpg"
+                                                alt="login form" style={{ "border-radius": "1rem 0 0 1rem;" }} />
+                                        </div>
+                                        <div className="col-md-4 col-lg-6 d-flex align-items-center">
+                                            <div className="card-body p-4 p-lg-5 text-black">
+
+                                                <form>
+
+                                                    <div className="d-flex align-items-center mb-3 pb-1">
+                                                        {/* <i className="fas fa-cubes fa-2x me-3" style={{"color": "#ff6219;"}}></i> */}
+                                                        <span className="h1 fw-bold mb-0" style={{ "text-align": "center" }}>ADD PRODUCT</span>
+                                                    </div>
 
 
-                                            <h1><u>Product Operation </u></h1>
-
-                                            <div className="form-row" >
-                                                {/* <div className="form-group">
-                <label htmlFor="f_name">Product Id:</label>
-                <input type="text" maxLength={"25"} name="productid" id="p_id" placeholder="product name" required onBlur={(event)=>{setProduct_id(event.target.value)}} />
-              </div> */}
-                                                <div className="form-outline mb-4">
-                                                    <label htmlFor="l_name">Product Name :</label>
-                                                    <input type="text" maxLength={"25"} placeholder="" name="p_name" id="p_n" required onBlur={(event) => { setProduct_name(event.target.value) }} />
-                                                </div>
-                                            </div>
 
 
-                                            <div className="form-group">
-                                                <label htmlFor="l_name">Category Name :</label>
-                                                <input type="text" maxLength={"25"} placeholder="" name="p_name" id="p_n" required onBlur={(event) => { setcategory_name(event.target.value) }} />
-                                            </div>
+                                                    <div className="form-outline mb-4">
+                                                        <label className="form-label" for="form2Example17">Product Name </label>
+                                                        <input type="text" id="form2Example17" maxLength={"25"} name="Pname" placeholder="Enter Product Name " className="form-control form-control-lg" required onChange={(e) => { setProduct_name(e.target.value) }} />
+
+                                                    </div>
 
 
-                                            <div className="form-row">
-                                                <div className="form-group">
-                                                    <label htmlFor="mobile">price :</label>
-                                                    <input type="number" maxLength={"14"} name="cost" id="c1"
-                                                        required onBlur={(event) => { setSelling_price(event.target.value) }} />
-                                                </div>
-                                            </div>
 
-                                            <div className="form-row">
-                                                <div className="form-group">
-                                                    <label htmlFor="mobile">Stock :</label>
-                                                    <input type="number" maxLength={"14"} name="stock" id="s1"
-                                                        required onBlur={(event) => { setstock(event.target.value) }} />
-                                                </div>
-                                            </div>
-                                            <div className="form-row">
-                                                <div className="form-group">
-                                                    <label htmlFor="mobile">Add image :<br></br></label>
-                                                    <input type="file"  name="image" id="img"
-                                                        required onChange={(event) => { setImage(event.target.files[0]) }} />
-                                                        {/* <button onClick={() =>axios.post('')}>Upload File</button> */}
-                                                </div>
-                                            </div>
+                                                    <div className="form-outline mb-4">
+                                                        <label className="form-label" for="form2Example17">Category Name </label>
+                                                        <input type="text" id="form2Example17" maxLength={"25"} name="Pname" placeholder="Enter Category Name " className="form-control form-control-lg" required onChange={(e) => { setcategory_name(e.target.value) }} />
+
+                                                    </div>
 
 
-                                            <div className="form-submit">
-                                                <input type="button" value="Submit Form" className="submit" name="submit" id="submit" onClick={addDataHandler} />
+
+                                                    <div className="form-outline mb-4">
+                                                        <label className="form-label" for="form2Example17">Selling Price </label>
+                                                        <input type="number" id="form2Example17" maxLength={"25"} name="cost" placeholder="Enter Selling Price " className="form-control form-control-lg" required onChange={(e) => { setSelling_price(e.target.value) }} />
+
+                                                    </div>
+
+                                                    <div className="form-outline mb-4">
+                                                        <label className="form-label" for="form2Example17">Stock </label>
+                                                        <input type="number" id="form2Example17" maxLength={"25"} name="cost" placeholder="Enter Stock " className="form-control form-control-lg" required onChange={(e) => { setstock(e.target.value) }} />
+
+                                                    </div>
+
+
+                                                    <div className="form-outline mb-4">
+                                                        <label className="form-label" for="form2Example17">Image </label>
+                                                        <input type="file" id="form2Example17" maxLength={"25"} name="cost" placeholder="Add Image " className="form-control form-control-lg" required onChange={(event) => { setImage(event.target.files[0]) }} />
+
+                                                    </div>
+
+                                                    <div className="pt-1 mb-4">
+                                                        <button className="btn btn-dark btn-lg btn-block" type="button" onClick={addDataHandler}>Submit</button>
+                                                    </div>
+                                                </form>
                                             </div>
                                         </div>
-
-                                        {/* <div className="d-flex align-items-center mb-3 pb-1">
-                                            <i className="fas fa-cubes fa-2x me-3" style={{ "color": "#ff6219;" }}></i>
-                                            <span className="h1 fw-bold mb-0" style={{ "text-align": "center" }}>Sign In</span>
-                                        </div>
-
-
-                                        <div className="pt-1 mb-4">
-                                            <button className="btn btn-dark btn-lg btn-block" type="button" >Sign In</button>
-                                        </div>
-
-                                        <a className="small text-muted" href="#!">Forgot password?</a>
-                                        <p className="mb-5 pb-lg-2" style={{ "color": "#393f81;" }}>Don't have an account? <a href="./Registeration"
-                                            style={{ "color": "#393f81;" }}>Register here</a></p>
- */}
-
                                     </div>
                                 </div>
                             </div>
                         </div>
+
                     </div>
-                </div>
-     
-            </section >
-    {/* <div className="main">
-    <div className="container-fluid" class="border border-primary mb-20">
-      {/* <div className="signup-content">
-        <div className="signup-img">
-          <img src="/assets/img/reg_form_images/female.jpg" alt="" />
-        </div> */}
-
-    {/* <div className="signup-form"> */ }
-    {/* <form method="POST" className="register-form" id="register-form"> */ }
-
-
-        </div >
-     
-
-            
-    )
+                </section>
+            </body>
+        </div>
+ )
 }
