8eeb838597357f06230cb44a7f38952217a3768f
Shivani W <48670700+shivaniw1234@users.noreply.github.com>
2022-09-22 15:12:10 +0530

    Add files via upload

--- SourceCode/BackEnd/DO/CartItemDO.java
+++ SourceCode/BackEnd/DO/CartItemDO.java
@@ -1,11 +1,38 @@
 package com.app.DO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartItemDO {
 	
-	private int cartItemId;
-	private int ProductId;
-	private int qty;
+	private int [] productId;
 	private int userId;
+	
+	public CartItemDO(int [] productId, int userId) {
+		super();
+		this.productId = productId;
+		this.userId = userId;
+	}
+
+	public int [] getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int [] productId) {
+		this.productId = productId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	
+
+	
 	
 	
 
