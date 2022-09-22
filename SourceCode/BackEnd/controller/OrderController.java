71a8906203977228908d1746748935edc1a0a144
rdrocks7 <106860440+rdrocks7@users.noreply.github.com>
2022-09-22 14:57:25 +0530

    Add files via upload

--- SourceCode/BackEnd/controller/OrderController.java
+++ SourceCode/BackEnd/controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.app.controller;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
+import org.hibernate.engine.transaction.jta.platform.internal.OC4JJtaPlatform;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,15 +12,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.DO.OrderDO;
 import com.app.entity.Address;
+import com.app.entity.Cart2;
 import com.app.entity.Order;
+import com.app.entity.Order2;
+import com.app.entity.Product;
+import com.app.entity.User;
 import com.app.repository.AddressRepository;
+import com.app.repository.Cart2Repository;
+import com.app.repository.CartRepository;
+import com.app.repository.Order2Repository;
+import com.app.repository.ProductRepository;
+import com.app.repository.UserRepository;
 import com.app.service.OrderService;
 
 @RestController
 @RequestMapping(path="order")
 public class OrderController {
 	
+	@Autowired
+	UserRepository urep;
+	
+	@Autowired	
+	Cart2Repository crep;
+	
+	@Autowired
+	ProductRepository prep;
 	
 	@Autowired
 	OrderService oserv;
@@ -26,12 +46,55 @@ public class OrderController {
 	@Autowired
 	AddressRepository arep;
 	
+	@Autowired
+	Order2Repository orep;
+	
 	@PostMapping("/add")
-	public boolean addOrder(@RequestBody Order o)
+	public boolean addOrder(@RequestBody OrderDO c)
 	{
-		Order o1=new Order(o.getUsers(),o.getOrderDate(),o.getOrderprice(),o.getDiscount(),o.getPaymentMode(),o.getPaymentStatus(),o.getProd(),o.getCarts());
-		oserv.PlaceOrder(o1);
-		return true;
+		
+		for(int i=0;i<c.getProductId().length;i++)
+		{
+			System.out.println("ids"+c.getProductId()[i]);
+			Order2 ord=new Order2(c.getOrderDate(),c.getPaymentMode(),c.getPaymentStatus());
+			ord.setUserId(c.getUserId());
+			Cart2 c2=crep.CheckCart(c.getUserId(), c.getProductId()[i]);
+			int qty=c2.getQty();
+			Product p=prep.findById(c.getProductId()[i]).get();
+			float cost=p.getSellingPrice();
+			float total=qty*cost;
+			ord.setOrderprice(total);
+			ord.setProductId(c.getProductId()[i]);
+			orep.save(ord);
+		
+		}
+		
+	return true;
+	
+		
+//		Order o1=new Order(o.getOrderDate(),o.getPaymentMode(),o.getPaymentStatus());
+//		User u=urep.findById(o.getUserId()).get();
+//		Float tamount=0.0f;
+//		
+//		
+//		if(u!=null)
+//		{
+//			List<Cart2> carts=crep.getUserCart(u.getId());
+//			for(int i = 0; i < carts.size(); i++){
+//			    int pid=carts.get(i).getProductId();
+//			    tamount+=carts.get(i).getQty() * prep.findById(pid).get().getSellingPrice();
+//			}
+//			o1.setOrderprice(tamount);
+//			o1.setUsers(u);
+//			oserv.PlaceOrder(o1);
+//			return true;
+//		}
+//		else
+//		{
+//			
+//			return false;
+//		}
+	
 	}
 	
 	@GetMapping("/history")
