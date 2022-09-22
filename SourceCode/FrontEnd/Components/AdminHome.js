3a698c9be7058fe575fa9248e8e486888bb60d58
Krishna Dope <106860635+krishnadope@users.noreply.github.com>
2022-09-22 15:04:47 +0530

    Add files via upload

---
+++ SourceCode/FrontEnd/Components/AdminHome.js
@@ -0,0 +1,46 @@
+
+
+import React from "react"; 
+import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
+
+function AdminHome(){
+
+return(
+
+<div >
+    <header className="full_bg">
+
+    <div className="header">
+       <div className="container-fluid">
+          <div className="row">
+             <div >
+                <div className="full">
+                   <div className="center-desk">
+                      <div className="logo">
+                         {/* <a href="index.html"><img src="assets/images/logo.png" alt="#" /></a> */}
+                      </div>
+                   </div>
+                </div>
+             </div>
+            
+          </div>
+       </div>
+    </div>
+
+  
+ </header>
+ </div>
+
+
+
+
+
+)
+
+
+
+
+}
+
+export default AdminHome;
