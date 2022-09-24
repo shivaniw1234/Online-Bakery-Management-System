7f2ec2ef3356b33d8cd1bc14057c2e6f350f5328
yadavrohit01087 <104022837+yadavrohit01087@users.noreply.github.com>
2022-09-24 18:24:39 +0530

    updated

--- SourceCode/FrontEnd/Components/CustomerProfile.js
+++ SourceCode/FrontEnd/Components/CustomerProfile.js
@@ -10,19 +10,45 @@ let[fname,setFname]=useState("");
 let[lname,setLname]=useState("");
 let[email,setEmail]=useState("");
 let[phone,setPhone]=useState("");
+let[image,setImage]=useState();
+let [add1,setAdd1]=useState("");
+let [add2,setAdd2]=useState("");
+let [city,setCity]=useState("");
+let [pincode,setPincode]=useState("");
 
 useEffect(() => {
 
    let uid= sessionStorage.getItem("userid");
    console.log("userid: ",uid);
+    // let address=sessionStorage.getItem("useraddress");
+    // console.log("address: ",address);
+    // if(address !=null){
+    //     setAdd(address);
+    // }
     axios.get(`http://localhost:8080/user/get/${uid}`,{}).
     then((res)=>{ setFname(res.data.firstName); 
         setLname(res.data.lastName); 
         setEmail(res.data.emailId);
         setPhone(res.data.phoneNo);
+        setImage(res.data.image);
         console.log("received profile",res.data);
 }).
     catch((error)=>{console.log(error);});
+
+
+   let id=sessionStorage.getItem("userid");
+    axios.get(`http://localhost:8080/order/getaddress/${id}`,{}).
+    then((res)=>{console.log("address",res.data); 
+    if(res.data !=null){
+      setAdd1(res.data.addressLine1);
+        setAdd2(res.data.addressLine2);
+         setCity(res.data.city);
+        setPincode(res.data.pincode);
+        console.log("city",city);
+
+    }
+
+}).catch((error)=>{console.log(error);});   
     // then((res)=>{setUser(users.filter((e)=>{ return e.emailId===sessionStorage.getItem("username")}))
  },[]);
     
@@ -36,12 +62,16 @@ return (
     <div id='ride' >
       <div className="card">
         <div className="card-body">
-        <img src="..\assets\images\yadavrohit0.jpg" style={{'borderRadius':'50%','height':'400px','width':'350px'}} />
+        <img src={`data:image/jpg;base64,${image}`} style={{'borderRadius':'50%','height':'400px','width':'350px'}}  alt="user-img" />
+        {/* <img src="..\assets\images\yadavrohit0.jpg"  /> */}
           <h1 className="card-title">{fname} {lname}</h1>
         </div>
         <ul className="list-group list-group-flush">
            <li className="list-group-item"><b>Email </b>: {email}</li>
            <li className="list-group-item"><b>Contact</b>: {phone}</li>
+           <li className="list-group-item"><b>Address</b>: {add1},
+           {add2} <br/> {city}-{pincode}</li>
+          
         </ul>
        
       </div>
